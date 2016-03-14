a7eead9244cd4e3205fa88d163eebe3991a42c08
Jigesh Parekh <jparekh117@gmail.com>
2016-03-14 14:26:03 -0700

    first

---
+++ Bloc/ToastIntro.rb
@@ -0,0 +1,27 @@
+
+puts "hello world" #method put that alters a value into a string and outputs to string object
+
+my_variable = "hello ruby" 
+p my_variable #outputs actual value of object
+
+#Math
+
+num1 = 4
+
+num2 = 5
+
+p num1 + num2 #outputs 9
+
+#Modulo
+
+p 30 % 5  # outputs 0 - modulo is the remainder from division
+
+#Task - Describe a toaster relative to a programming method
+
+=begin
+	Toaster
+		A Toaster is a black box that takes in carb arguments (slice of bread, half of a bagel)
+		The return statement from the toaster method is the carb argument heated to perfection (toastified)
+		Side effects for the Toast method may return crumbs 
+		placing an unexpected non carb based argument within the toast method may break the method, outputting a dangerous unintended event, such as shock
+=end
